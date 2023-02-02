4565d270272940c0c3a7892ebde69c7c5d44de60
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-02-02 12:52:43 +0100

    :sparkles: Fond + FullScreen

--- code/PongClient/Game1.cs
+++ code/PongClient/Game1.cs
@@ -8,10 +8,18 @@ namespace PongClient
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _backgroundTexture;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -26,14 +34,31 @@ namespace PongClient
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _backgroundTexture = Content.Load<Texture2D>("fond");
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.F11) )
+            {
+                if (_graphics.IsFullScreen)
+                {
+                    _graphics.PreferredBackBufferWidth = 1080;
+                    _graphics.PreferredBackBufferHeight = 720;
+                }
+                else
+                {
+                    _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                    _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+                }
+
+                _graphics.IsFullScreen = !_graphics.IsFullScreen;
+                _graphics.ApplyChanges();
+            }
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
 
             // TODO: Add your update logic here
 
@@ -42,7 +67,11 @@ namespace PongClient
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Beige);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backgroundTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
