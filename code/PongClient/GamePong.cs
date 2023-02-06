887c19ebb79c6911518e9b559c837e0226b6bdef
loperret3 <loris.perret@etu.uca.fr>
2023-02-06 19:47:55 +0100

    :sparkles: mise en place

--- code/PongClient/GamePong.cs
+++ code/PongClient/GamePong.cs
@@ -3,8 +3,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using PongClient.States;
-using PongClient.Stats;
 
 namespace PongClient
 {
