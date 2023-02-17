a55e9990fc40a4c12f163981f87329dd67e3c689
Noan07 <84435602+Noan07@users.noreply.github.com>
2023-02-17 14:12:55 +0100

    :hammer: Ajout de classes au sein du modele

---
+++ code/Modele/CapacityBall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modele
+{
+    //ici la velocity correspond à la vitesse de la balle
+    public class CapacityBall
+    {
+        private float size;
+        private float velocity;
+
+        public CapacityBall(float size, float velocity)
+        {
+            this.size = size;
+            this.velocity = velocity;
+        }
+
+        public void ChangeSize(float newSize)
+        {
+            this.size = newSize;
+        }
+
+        public void ChangeVelocity(float newVelocity)
+        {
+            this.velocity = newVelocity;
+        }
+    }
+}
---
+++ code/Modele/WebSocket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modele
+{
+
+    public class WebSocket
+    {
+        public void send()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void recive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
