a3b6f61f664b4f0aba72436d2da7d6f201ffe83e
rakhedair <Rami.KHEDAIR@etu.uca.fr>
2023-02-17 14:47:08 +0100

    🔨 comploter de classes

--- code/Modele/IControlMouvement/LeapMotion.cs
+++ code/Modele/IControlMouvement/LeapMotion.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Modele.IControlMouvement
 {
-    internal class LeapMotion
+    internal class LeapMotion : IControlMouvement
     {
+        public float getCoordonate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- code/Modele/IControlMouvement/Mouse.cs
+++ code/Modele/IControlMouvement/Mouse.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+//using MonoGame.Extended; est ce que je me met en dependance de monogame
+
 namespace Modele.IControlMouvement
 {
-    internal class Mouse
+    public class Mouse : IControlMouvement
     {
+
+        public float getCoordonate()
+        {
+            //return MouseExtended.GetState().Position.Y;
+        }
     }
+
 }
