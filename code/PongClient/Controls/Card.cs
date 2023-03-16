204a3d17b81c3f952eaab8a87fa7679949e34dfc
loperret3 <loris.perret@etu.uca.fr>
2023-03-16 19:41:18 +0100

    :lipstick: Ecran fin de partie

---
+++ code/PongClient/Controls/Card.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongClient.Controls
+{
+    public class Card : Component
+    {
+        public Sprite Texture;
+        public Vector2 Position;
+        public RectangleF Zone => Texture.GetBoundingRectangle(Position, 0, Vector2.One);
+
+        public Card(Sprite texture, Vector2 position)
+        {
+            Texture = texture;
+            Position = position;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, 0, Vector2.One);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
--- code/PongClient/Screens/EndPartyScreen.cs
+++ code/PongClient/Screens/EndPartyScreen.cs
@@ -6,13 +6,17 @@ using Modele.EntityPackage;
 using Modele.MovementPackage.MotionSensorPackage;
 using Modele.PlayerPackage;
 using MonoGame.Extended;
+using MonoGame.Extended.Sprites;
+using PongClient.Controls;
 using PongClient.Screens.MenuPackage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Game = Modele.GamePackage.Game;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace PongClient.Screens
 {
@@ -22,6 +26,13 @@ namespace PongClient.Screens
 
         private readonly Game _pongGame;
         private SoundEffectInstance _musicInstance; // Instance de la musique
+
+        private Button newGameButton;
+        private Card localPlayerRectangle;
+        private Card externalPlayerRectangle;
+        private Card rectangle1;
+        private Card rectangle2;
+        private Card rectangle3;
 
         public EndPartyScreen(GamePong game, Game pongGame)
           : base(game)
@@ -41,7 +52,25 @@ namespace PongClient.Screens
             _musicInstance = music.CreateInstance();
             _musicInstance.IsLooped = true; // Lecture en boucle
             _musicInstance.Play(); // Démarrage de la musique
+
+            var playerRectangleTexture = new Sprite(Content.Load<Texture2D>("Form/sheet"));
+            var rectangle1Texture = new Sprite(Content.Load<Texture2D>("Form/rectangle1"));
+            var rectangle2Texture = new Sprite(Content.Load<Texture2D>("Form/rectangle2"));
+            var rectangle3Texture = new Sprite(Content.Load<Texture2D>("Form/rectangle3"));
+
+            localPlayerRectangle = new Card(playerRectangleTexture, new Vector2(_widthCenter / 2, _heightCenter / 2));
+            externalPlayerRectangle = new Card(playerRectangleTexture, new Vector2(_widthCenter + _widthCenter / 2, _heightCenter / 2));
+
+            rectangle1 = new Card(rectangle1Texture, new Vector2(_widthCenter, localPlayerRectangle.Zone.Bottom + rectangle1Texture.TextureRegion.Height));
+            rectangle2 = new Card(rectangle2Texture, new Vector2(rectangle1.Zone.Left - rectangle1Texture.TextureRegion.Width, localPlayerRectangle.Zone.Bottom + rectangle1Texture.TextureRegion.Height));
+            rectangle3 = new Card(rectangle3Texture, new Vector2(rectangle1.Zone.Right + rectangle1Texture.TextureRegion.Width, localPlayerRectangle.Zone.Bottom + rectangle1Texture.TextureRegion.Height));
+
+            var newGameButtonTexture = new Sprite(Content.Load<Texture2D>("Form/newGameButton"));
+
+            newGameButton = new Button(newGameButtonTexture, new Vector2(_widthCenter, rectangle1.Zone.Bottom + rectangle1Texture.TextureRegion.Height));
+            newGameButton.Click += NewGameButton_Click;
         }
+            
 
         public override void Draw(GameTime gameTime)
         {
@@ -49,7 +78,32 @@ namespace PongClient.Screens
 
             _spriteBatch.Draw(_backgroundTexture, new Vector2(0, 0), Color.White);
 
+            DrawParty(gameTime, _pongGame);
+            DrawPlayer(gameTime, _pongGame.LocalPlayer, localPlayerRectangle);
+            DrawPlayer(gameTime, _pongGame.ExternalPlayer, externalPlayerRectangle);
+
+            newGameButton.Draw(gameTime, _spriteBatch);
+
             _spriteBatch.End();
+        }
+
+        public void DrawPlayer(GameTime gameTime, Player player, Card playerCard)
+        {
+            playerCard.Draw(gameTime, _spriteBatch);
+
+        }
+
+        public void DrawParty(GameTime gameTime, Game game)
+        {
+            rectangle1.Draw(gameTime, _spriteBatch);
+            rectangle2.Draw(gameTime, _spriteBatch);
+            rectangle3.Draw(gameTime, _spriteBatch);
+
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            ScreenManager.LoadScreen(new MenuGameMode(_game));
         }
 
         public override void Update(GameTime gameTime)
@@ -60,6 +114,8 @@ namespace PongClient.Screens
                 _musicInstance.Stop();
                 ScreenManager.LoadScreen(new MenuHome(_game));
             }
+
+            newGameButton.Update(gameTime);
         }
     }
 }
