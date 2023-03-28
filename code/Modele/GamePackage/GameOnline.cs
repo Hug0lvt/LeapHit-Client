104d9d6b610c7586d9088ac3809a54eaaf2c6f24
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-03-28 11:57:22 +0200

    ball

--- code/Modele/GamePackage/GameOnline.cs
+++ code/Modele/GamePackage/GameOnline.cs
@@ -59,8 +59,8 @@ namespace Modele.GamePackage
                 Tuple<float, float> ballReceive = datas.Ball;
 
                 // Set coordonate
-                /*ball.X = ballReceive.Item1;
-                ball.Y = ballReceive.Item2;*/
+                ball.X = ballReceive.Item1;
+                ball.Y = ballReceive.Item2;
 
                 // Move
                 externalPlayer.Paddle.Move(playerReceive, screenHeight, screenWidth);
@@ -112,7 +112,7 @@ namespace Modele.GamePackage
             localPlayer.Paddle.Move(localPlayer.StrategieMovement.GetMovement(), screenHeight, screenWidth);
             //externalPlayer.Paddle.Move(playerReceive, screenHeight, screenWidth);
                     
-            //ball.Move(elapsedSecond, screenHeight, screenWidth);
+            ball.Move(elapsedSecond, screenHeight, screenWidth);
 
             //SetScore(ball, screenWidth, screenHeight, elapsedSecond);
 
